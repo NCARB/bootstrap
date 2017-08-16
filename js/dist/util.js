a522b7281bf51a06c65fdae32d555f04f1fc66e8
Dave Anderson <danderson@ncarb.org>
2017-08-16 09:49:49 -0400

    feat(version): v4.0.0-beta.1+twbs4.0.0-beta

--- js/dist/util.js
+++ js/dist/util.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v4.0.0-beta): util.js
+ * Bootstrap (v4.0.0-beta.1+twbs4.0.0-beta): util.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
